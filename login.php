f85e2a6a305947add9cfeacf61d95e78bd0cb2d1
ZSR-Blxck <zavierrattray@gmail.com>
2021-11-29 23:32:29 -0500

    Login Page progress

--- login.php
+++ login.php
@@ -8,8 +8,6 @@ $count = 0;
 
       $email = $_POST['email'];
       $_password =$_POST['password'];
-      $password_digest = password_hash($_password, PASSWORD_DEFAULT);
-      $pass_verify= password_verify($_password, $password_digest);
 try{
  	$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 	    $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
@@ -17,20 +15,19 @@ try{
 	} catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 	}
-	if ( password_verify($_password, $password_digest)){
-		 $ses_sql ="SELECT email FROM Users WHERE email = '$email'";
-   		$statement = $conn->prepare($ses_sql);
-   		$statement->execute(array('email'=> $_POST["email"],'password'=>$_POST["password"]));
-      
-	}
-     
-      $count = $statement -> rowCount();
-		
-      if($count > 0) {
-        $_SESSION['login_user'] = $email; 
-        header( "Location: register.html");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-         echo nl2br ("$error \r\n");
-      }
+    $result = $conn-> prepare("SELECT * FROM Users WHERE email = '$email'");
+
+    $result -> execute(array('email'=> $_POST["email"]));
+
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    if(is_array($row))
+    {
+        if ( password_verify($_password,$row['password'])){
+            $_SESSION['login_user'] = $email; 
+            header( "Location: register.html");
+          }else {
+             $error = "Your Login Name or Password is invalid";
+             echo nl2br ("$error \r\n");
+          }
+    }
 }
