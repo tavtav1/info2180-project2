1aaba4a537284ea0dc74e9738a3bb042bc7ef37f
ZSR-Blxck <zavierrattray@gmail.com>
2021-11-30 00:08:50 -0500

    working Login and Registration needs Ajax and JS

--- userData.php
+++ userData.php
@@ -18,45 +18,45 @@ $err = array();
 if($_SERVER["REQUEST_METHOD"]=="POST")
 {
 	try {
-	// CHECKS IF FIELD IS EMPTY
-	if (empty($_POST["fname"])) 
-	{
-		// INSERTS ERROR INTO ARRRAY
-		array_push($err, $fnameErr);
-	}
-
-	// CHECKS IF FIELD IS EMPTY
-  	if (empty($_POST["lname"])) 
-  	{
-		// INSERTS ERROR INTO ARRRAY
-		array_push($err, $lnameErr);
-	}
-
-	// CHECKS IF FIELD IS EMPTY
-    if (empty($_POST["email"]))
-    {
-		// INSERTS ERROR INTO ARRRAY
-		array_push($err, $emailErr);
-	}
-
-	// CHECKS IF FIELD IS EMPTY
-    if (empty($_POST["Password"])) 
-    {
-		// INSERTS ERROR INTO ARRRAY
-		array_push($err, $PasswordErr);
-	}else 
-	{
-		// CHECKS IF VALUE WAS NOT ALPHANUMERIC 
-		if(!preg_match("/^[a-zA-Z0-9]*$/", $password))
+		// CHECKS IF FIELD IS EMPTY
+		if (empty($_POST["fname"])) 
 		{
 			// INSERTS ERROR INTO ARRRAY
-			array_push($err, $passwordErr);
-		}else{
-			$password = $_POST["password"];
+			array_push($err, $fnameErr);
 		}
-	}
-} catch ( \Exception $e) {
-	header( "Location: register.html");
+
+		// CHECKS IF FIELD IS EMPTY
+	  	if (empty($_POST["lname"])) 
+	  	{
+			// INSERTS ERROR INTO ARRRAY
+			array_push($err, $lnameErr);
+		}
+
+		// CHECKS IF FIELD IS EMPTY
+	    if (empty($_POST["email"]))
+	    {
+			// INSERTS ERROR INTO ARRRAY
+			array_push($err, $emailErr);
+		}
+
+		// CHECKS IF FIELD IS EMPTY
+	    if (empty($_POST["password"])) 
+	    {
+			// INSERTS ERROR INTO ARRRAY
+			array_push($err, $passwordErr);
+		}else 
+		{
+			// CHECKS IF VALUE WAS NOT ALPHANUMERIC 
+			if(!preg_match("/^[a-zA-Z0-9]*$/", $password))
+			{
+				// INSERTS ERROR INTO ARRRAY
+				array_push($err, $passwordErr);
+			}else{
+				$password = $_POST["password"];
+			}
+		}
+	} catch ( \Exception $e) {
+		header( "Location: register.html");
 	}
 }	
 
@@ -71,9 +71,8 @@ if (sizeof($err)==0) {
 			} catch (PDOException $pe) {
 		    die("Could not connect to the database $dbname :" . $pe->getMessage());
 		}
-}
 
 $insertData = "INSERT INTO Users(firstname,lastname,password,email,date_joined) VALUES('$fname','$lname','$password_digest','$email','$date_joined')";
 
 $conn->exec($insertData);
-
+}
